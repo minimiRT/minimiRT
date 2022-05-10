74a72ff9cffb037c1b5d44aedee42304068c3986
refigo <wlguq100@naver.com>
2022-05-10 19:11:41 +0900

    feat: add drive_ray_tracing function to tracing.h

--- srcs/tracing/tracing.h
+++ srcs/tracing/tracing.h
@@ -3,6 +3,7 @@
 
 # include "scene.h"
 
+void		drive_ray_tracing(t_scene *scene);
 t_bool		hit_objects(t_scene *scene, t_ray ray, t_hit_record *record);
 t_color3	get_color_from_phong_lighting(t_scene *scene, t_ray pixel_ray, t_hit_record record);
 void		draw_pixel(t_scene *scene, t_color3 color, double u, double v);
