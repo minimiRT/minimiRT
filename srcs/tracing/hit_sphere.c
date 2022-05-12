33f3347eae756de03b56ff9086ec3363644bcddc
refigo <wlguq100@naver.com>
2022-05-12 17:50:50 +0900

    refactor: modify hit_sphere code for norm

--- srcs/tracing/hit_sphere.c
+++ srcs/tracing/hit_sphere.c
@@ -1,44 +1,61 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hit_sphere.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 17:27:40 by mgo               #+#    #+#             */
+/*   Updated: 2022/05/12 17:27:42 by mgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "tracing.h"
 #include <math.h>
 
-t_bool	hit_sphere(t_object *object, t_trace *tracing)
+static t_bool	find_and_set_root_of_sphere(t_sphere *sphere, t_trace *tracing, \
+											t_vec3 vec_co, double *root)
 {
-	t_sphere	*sphere;
-	t_vec3		co;
 	double		a;
 	double		half_b;
 	double		c;
 	double		discriminant;
 	double		sqrtd;
-	double		root;
 
-	sphere = object->element;
-	co = sub_vec3(tracing->ray.origin, sphere->center);
 	a = dot_vec3(tracing->ray.direction, tracing->ray.direction);
-	half_b = dot_vec3(tracing->ray.direction, co);
-	c = dot_vec3(co, co) - sphere->radius_square; 
+	half_b = dot_vec3(tracing->ray.direction, vec_co);
+	c = dot_vec3(vec_co, vec_co) - sphere->radius_square; 
 	discriminant = half_b * half_b - a * c;
 	if (discriminant < 0)
 		return (FALSE);
 	sqrtd = sqrt(discriminant);
-	root = (-half_b - sqrtd) / a;
-	if (root < tracing->record.min || tracing->record.max < root)
+	*root = (-half_b - sqrtd) / a;
+	if (*root < tracing->record.min || tracing->record.max < *root)
 	{
-		root = (-half_b + sqrtd) / a;
-		if (root < tracing->record.min || tracing->record.max < root)
+		*root = (-half_b + sqrtd) / a;
+		if (*root < tracing->record.min || tracing->record.max < *root)
 			return (FALSE);
 	}
+	return (TRUE);
+}
+
+t_bool	hit_sphere(t_object *object, t_trace *tracing)
+{
+	t_sphere	*sphere;
+	t_vec3		vec_co;
+	double		root;
+
+	sphere = object->element;
+	vec_co = sub_vec3(tracing->ray.origin, sphere->center);
+	if (find_and_set_root_of_sphere(sphere, tracing, vec_co, &root) == FALSE)
+		return (FALSE);
 	tracing->record.distance_from_ray_origin = root;
 	tracing->record.hit_point = get_point_ray_reach(tracing->ray, root);
-	tracing->record.normal = div_vec3_t(sub_vec3(tracing->record.hit_point, sphere->center), sphere->radius); // get_unit으로 바꿀것
-
-	// set normal
+	tracing->record.normal = get_unit_vec3(sub_vec3(tracing->record.hit_point, sphere->center));
+	// todo: rename front_face to is_front_face
 	tracing->record.front_face = (dot_vec3(tracing->ray.direction, tracing->record.normal) < 0);
 	if (tracing->record.front_face == FALSE)
 		tracing->record.normal = mul_vec3_t(tracing->record.normal, -1);
-
 	tracing->record.albedo = object->albedo;
 	return (TRUE);
 }
--- srcs/tracing/tracing.h
+++ srcs/tracing/tracing.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tracing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 17:27:58 by mgo               #+#    #+#             */
+/*   Updated: 2022/05/12 17:27:59 by mgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef TRACING_H
 # define TRACING_H
 
@@ -7,10 +19,10 @@
 # include "mlx.h"
 
 void		drive_ray_tracing(t_scene *scene);
-t_bool		hit_objects(t_objects *objects, t_trace *tracing);
 t_color3	get_color_from_phong_lighting(t_scene *scene, t_trace *tracing);
 void		draw_pixel(t_scene *scene, t_pixel *pixel);
 
+t_bool		hit_objects(t_objects *objects, t_trace *tracing);
 t_bool		hit_sphere(t_object *object, t_trace *tracing);
 t_bool		hit_plane(t_object *object, t_trace *tracing);
 
