89c2a84f8fb94d2f1fa0be7b574da426c8d0e077
refigo <wlguq100@naver.com>
2022-05-13 09:45:30 +0900

    refactor: modify tracing.h for organizing function prototype

--- srcs/tracing/tracing.h
+++ srcs/tracing/tracing.h
@@ -6,7 +6,7 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 17:27:58 by mgo               #+#    #+#             */
-/*   Updated: 2022/05/13 09:11:37 by mgo              ###   ########.fr       */
+/*   Updated: 2022/05/13 09:44:39 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,9 @@
 # include "pixel.h"
 # include "mlx.h"
 
-
 void		drive_ray_tracing(t_scene *scene);
+t_bool		hit_objects(t_objects *objects, t_trace *tracing);
 t_color3	get_color_from_phong_lighting(t_scene *scene, t_trace *tracing);
 void		draw_pixel(t_scene *scene, t_pixel *pixel);
-
-t_bool		hit_objects(t_objects *objects, t_trace *tracing);
-t_bool		hit_sphere(t_object *object, t_trace *tracing);
-t_bool		hit_plane(t_object *object, t_trace *tracing);
 
 #endif
