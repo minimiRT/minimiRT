503409903cff86f7f5fc09453ee37fd5ac1400b5
Myeonghyeon Park <mypark@c1r2s2.42seoul.kr>
2022-05-13 13:59:44 +0900

    fix: Fix decision of hit_point between side and circle

--- srcs/tracing/tracing_object/hit_cylinder/hit_cylinder.c
+++ srcs/tracing/tracing_object/hit_cylinder/hit_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 09:06:10 by mypark            #+#    #+#             */
-/*   Updated: 2022/05/13 13:39:39 by mypark           ###   ########.fr       */
+/*   Updated: 2022/05/13 13:52:28 by mypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,15 @@ t_bool	hit_cylinder(t_object *object, t_trace *tracing)
 {
 	t_cylinder	*cylinder;
 	t_vec3		vec_co;
+	t_bool		hit_side;
+	t_bool		hit_circle;
 
 	cylinder = object->element;
 	vec_co = sub_vec3(tracing->ray.origin, cylinder->center);
-	if (hit_cylinder_side(cylinder, tracing, vec_co) == TRUE)
-	{
-		tracing->record.albedo = object->albedo;
-		return (TRUE);
-	}
-	if (hit_cylinder_circle(cylinder, tracing, vec_co) == TRUE)
-	{
-		tracing->record.albedo = object->albedo;
-		return (TRUE);
-	}
-	return (FALSE);
+	hit_circle = hit_cylinder_circle(cylinder, tracing, vec_co);
+	hit_side = hit_cylinder_side(cylinder, tracing, vec_co);
+	if (hit_circle == FALSE && hit_side == FALSE)
+		return (FALSE);
+	tracing->record.albedo = object->albedo;
+	return (TRUE);
 }
