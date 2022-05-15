570ecb5977f0a3cf6cedd46b03634b72aefcd6be
Myeonghyeon Park <mypark@c1r2s6.42seoul.kr>
2022-05-15 14:05:44 +0900

    chore: Edit msg in get_unit_vec3.c

--- srcs/structure/vec3/get_unit_vec3.c
+++ srcs/structure/vec3/get_unit_vec3.c
@@ -6,7 +6,7 @@
 /*   By: mypark <mypark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:52:40 by mgo               #+#    #+#             */
-/*   Updated: 2022/05/09 21:27:10 by mypark           ###   ########.fr       */
+/*   Updated: 2022/05/15 14:05:31 by mypark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_vec3	get_unit_vec3(t_vec3 vec)
 	double	len;
 
 	len = get_vec3_len(vec);
-	ft_assert(len != 0, "Assert: division by zero in get_unit_vec3\n");
+	ft_assert(len != 0, "Error: division by zero in get_unit_vec3\n");
 	vec.x /= len;
 	vec.y /= len;
 	vec.z /= len;
